96000f628b09a23b9df7595839f0449e5b3e80d9
vergoraf <60448040+vergoraf@users.noreply.github.com>
2020-10-28 12:43:24 +0100

    Update weighting_values.R

--- weighting_values.R
+++ weighting_values.R
@@ -5,7 +5,7 @@ library(sf)
 
 #2.-to import silage maize data
 load("rda/silage_maize_cat.rda")
-a<-silage_maize_cat[[1]]
+
 #3.-to give the weight values
 weight.ca<-0.527
 weight.livestock<-0.217
@@ -19,16 +19,12 @@ for(i in 1:length(silage_maize_cat)){
                                        (ifelse(!is.na(silage_maize_cat[[i]]$cat_liv),(silage_maize_cat[[i]]$cat_liv*weight.livestock),0)) +
                                        (ifelse(!is.na(silage_maize_cat[[i]]$cat_pers),(silage_maize_cat[[i]]$cat_pers*weight.persistency),0)) +
                                        (ifelse(!is.na(silage_maize_cat[[i]]$cat_size),(silage_maize_cat[[i]]$cat_size*weight.sizefarmy),0)))
-  #silage_maize_cat[[i]]$cat_total<-ifelse(silage_maize_cat[[i]]$cat_size==0,0,silage_maize_cat[[i]]$cat_total)#to leave outside parcels less than 350 ha
 }
 
 #5.-summarize data  in one file and establish categorization values
 outcome_smaize<-do.call("rbind",silage_maize_cat)
 outcome_smaize$area<-st_area(outcome_smaize)
-outcome_smaize$cat_label<-ifelse(outcome_smaize$cat_total>=0 & outcome_smaize$cat_total<20,"very unlikely",
-                                 ifelse(outcome_smaize$cat_total>=20 & outcome_smaize$cat_total<40,"unlikely",
-                                 ifelse(outcome_smaize$cat_total>=40 & outcome_smaize$cat_total<60,"medium likelihood",
-                                 ifelse(outcome_smaize$cat_total>=60 & outcome_smaize$cat_total<80,"likely","very likely"))))
+outcome_smaize$cat_label<-ifelse(outcome_smaize$cat_total>=55,"Likely","Unlikely")
  
 #6.-Exports                           
 #export final results of silage maize categories asone shapefile
