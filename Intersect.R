c756475ee3b7921510f521ca8ba689e6354418d1
vergoraf <60448040+vergoraf@users.noreply.github.com>
2020-10-28 11:52:12 +0100

    Update Intersect.R

--- Intersect.R
+++ Intersect.R
@@ -50,76 +50,9 @@ for(i in 1:length(s_maize_post2007)){
 save(s_maize_post2007, file = "rda/s_maize_post2007.rda")
 load(file = "rda/s_maize_post2007.rda")#only if it is necessary.
 
-
-
-##VERSION A
-
-#7.-intersection 
-#Intersect of Silage maize with the WEC polygons.
-#Adjusting projections
-for( i in 1:11){
-  s_maize_post2007[[i]]<-st_transform(s_maize_post2007[[i]],32633)
-}
-#start intersection
-s_maize_intersect <- list()
-for (i in 1:length(s_maize_post2007)) {
-  s_maize_intersect[[i]] <- st_intersection(s_maize_post2007[[i]],buffer.list[[i]])
-}
-
-#8.- Save Intersect outcome
-save(s_maize_intersect, file = "rda/s_maize_intersect.rda")
-#load(file = "rda/s_maize_intersect.rda")# only if it is necessary.
-
-#9.- Area calculation
-# here it is calculated the area and then selected the max area for x id 
-#for example: it can be that one id (plot) it is crossed by two or more categories
-#so it is selected which more area has. 
-#it calculates area
-for (i in 1:length(s_maize_intersect)) {
-  s_maize_intersect[[i]]$area <-st_area(s_maize_intersect[[i]])#in square meters
-}
-#Aggregate id.
-selected_s_maize_aggr<-list()
-s.maize_list<-list()
-for (j in 1:length(s_maize_intersect)) {
-  s.maize_list[[j]]<-as.data.frame(s_maize_intersect[[j]][,c("id","cat","area")])
-  selected_s_maize_aggr[[j]]<- aggregate(x=s.maize_list[[j]]["area"],
-                                         by=s.maize_list[[j]][c("cat","id")],
-                                         FUN=sum)
-  names(selected_s_maize_aggr[[j]])<- c("cat","id","area_CA")#create name Catchment area
-}
-
-
-#it calculates area of all the silage maize plots
-for (i in 1:length(s_maize_post2007)) {
-  s_maize_post2007[[i]]$area <-st_area(s_maize_post2007[[i]])#in square meters
-}
-
-
-#10.- joining table and calculate proportion
-silage_maize_cat<-list()
-for (i in 1:length(s_maize_post2007)){
-  silage_maize_cat[[i]]<-left_join(s_maize_post2007[[i]],selected_s_maize_aggr[[i]],by="id")
-  silage_maize_cat[[i]]$cat_CA<-as.numeric(silage_maize_cat[[i]]$area_CA/silage_maize_cat[[i]]$area*100)
-}
-
-
-#11.- save silage maize data added the cat WEC.
-save(silage_maize_cat, file = "rda/silage_maize_cat.rda")
-#load(file = "rda/silage_maize_cat.rda")#only if it is necessary.
-
-#12.- testing and plotting 
-tmap_mode('view')
-tm_shape(silage_maize_cat[[1]]) +
-  tm_polygons("cat_CA",palette="Spectral",style="equal",border.col = NA)+
-  tm_layout(frame=T, title="2008")
-
-
-##VERSION B
-
 #7.-intersection 
 load("rda/d_Pol_WEC.rda")
-#Intersect of Silage maize with the WEC polygons.
+#Intersect of Silage maize with the Catchment area polygons.
 #Adjusting projections
 for( i in 1:11){
   s_maize_post2007[[i]]<-st_transform(s_maize_post2007[[i]],32633)
@@ -170,26 +103,26 @@ for (i in 1:length(s_maize_post2007)){
 #11.-Save Proportion
 save(s_proportion,file="rda/s_proportion.rda")
 
-#Just in  case is the first variable to create
-#11.- joining table and calculate proportion
-#silage_maize_cat<-list()
-#for (i in 1:length(s_maize_post2007)){
-#  silage_maize_cat[[i]]<-left_join(s_maize_post2007[[i]],s_proportion[[i]],by="id")
-#}
+
+#12.- joining table and calculate proportion
+silage_maize_cat<-list()
+for (i in 1:length(s_maize_post2007)){
+  silage_maize_cat[[i]]<-left_join(s_maize_post2007[[i]],s_proportion[[i]],by="id")
+}
 
 
-#12.-joining the cat_CA_B to the silage maize selected
+#13.-joining the cat_CA_B to the silage maize selected
 load("rda/silage_maize_cat.rda")
 for (i in 1:length(silage_maize_cat)){
   silage_maize_cat[[i]]<-left_join(silage_maize_cat[[i]],s_proportion[[i]],by="id")
 }
 
 
-#13.- save silage maize data added the cat WEC.
+#14.- save silage maize data added the cat WEC.
 save(silage_maize_cat, file = "rda/silage_maize_cat.rda")
 #load(file = "rda/silage_maize_cat.rda")#only if it is necessary.
 
-#14.- testing and plotting 
+#15.- testing and plotting 
 tmap_mode('view')
 tm_shape(silage_maize_cat[[6]]) +
   tm_polygons("cat_CA",palette="Spectral",style="equal",border.col = NA)+
